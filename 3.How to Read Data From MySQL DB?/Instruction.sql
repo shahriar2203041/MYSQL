9b2c9a8f2d5c3802d3edfe0bfd7d9bf41ff48fce
Shahriar Kamal <shahriar2203041@gmail.com>
2025-04-08 00:40:51 +0600

    Create Instruction.sql

---
+++ 3.How to Read Data From MySQL DB?/Instruction.sql
@@ -0,0 +1,11 @@
+To retrieve data from the students table, you can use the following SQL commands:
+
+-- Show all columns and all rows
+SELECT * FROM students;
+
+-- Show only name and id columns from all rows
+SELECT name, id FROM students;
+
+-- Show all columns from the row where id is 41
+SELECT * FROM students WHERE id = 41;
+These commands will help you view specific or complete data from the students table.
